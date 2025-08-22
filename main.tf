6080f64d7eb4da36d20f7881d72ccc8946a8df7d
Gyovane souza <gyovanesouzza@outlook.com.br>
2025-08-21 21:25:45 -0300

    remove var name

--- main.tf
+++ main.tf
@@ -142,5 +142,5 @@ resource "aws_api_gateway_deployment" "api_deploy" {
     aws_api_gateway_integration.sync_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.api_cloud.id
-  stage_name  = "dev"
+  #stage_name  = "dev"
 }
